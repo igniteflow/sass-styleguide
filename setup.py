02c82378cf681f6be07c10cb67f981ad8d541d4c
Phil Tysoe <philtysoe@gmail.com>
2016-11-08 23:33:29 +0000

    Missing files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 setup(
     name='sass-styleguide',
-    version='0.1',
+    version='0.1.1',
     description='Generates a static html page displaying information about your Sass files',
     author='Phil Tysoe',
     author_email='philtysoe@gmail.com',
